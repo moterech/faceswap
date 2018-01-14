a779a1866d22da84f54951f9ccaad37ae5178e66
moterech <14049998+moterech@users.noreply.github.com>
2018-01-14 20:50:39 +0000

    Add support for loading images from GCS

--- lib/utils.py
+++ lib/utils.py
@@ -1,8 +1,8 @@
-import cv2
 import numpy
-
 from pathlib import Path
-from scandir import scandir
+from cStringIO import StringIO
+from PIL import Image
+from tensorflow.python.lib.io import file_io
 
 
 def get_folder(path):
@@ -12,11 +12,19 @@ def get_folder(path):
 
 
 def get_image_paths(directory):
-    return [x.path for x in scandir(directory) if x.name.endswith('.jpg') or x.name.endswith('.jpeg') or x.name.endswith('.png')]
+    return ['{}/{}'.format(directory, x) for x in file_io.list_directory(directory) if x.endswith('.jpg') or x.endswith('.jpeg') or x.endswith('.png')]
+
+
+def load_image(path):
+    handler = StringIO()
+    handler.write(file_io.read_file_to_string(path, binary_mode=True))
+    image = numpy.array(Image.open(handler))
+    handler.close()
+    return image
 
 
 def load_images(image_paths, convert=None):
-    iter_all_images = (cv2.imread(fn) for fn in image_paths)
+    iter_all_images = (load_image(fn) for fn in image_paths)
     if convert:
         iter_all_images = (convert(img) for img in iter_all_images)
     for i, image in enumerate(iter_all_images):
